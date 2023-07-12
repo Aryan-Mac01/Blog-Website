e44989bd95d64e8b3a3ecdd1b36ee8c53bf12d9c
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2023-07-12 22:48:00 +0530

    compose

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+const posts = [];
 
 app.set('view engine', 'ejs');
 
@@ -18,7 +19,7 @@ app.use(express.static("public"));
 
 
 app.get("/", function(req, res){
-  res.render("home", {StartingContent: homeStartingContent });
+  res.render("home", {StartingContent: homeStartingContent, posts: posts });
 });
 
 app.get("/about", function(req, res){
@@ -28,6 +29,21 @@ app.get("/about", function(req, res){
 app.get("/contact", function(req, res){
   res.render("contact", {ContactContent: contactContent});
 });
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+let object = {
+  title: req.body.postTitle,
+  post: req.body.postBody
+};
+posts.push(object);
+
+res.redirect("/");
+});
+
 
 
 
